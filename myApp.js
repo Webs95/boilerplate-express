ee1ade91f5de06cc75d4ab44142c8940ef1f77c9
Webs95 <alexandersemashko1995@yandex.ru>
2024-02-23 20:25:11 +0300

    add 4 solution

--- myApp.js
+++ myApp.js
@@ -11,6 +11,9 @@ app.get('/get-file', (req, res) => {
  res.sendFile(filePath);
 })
 
+app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
+
 
 
 
